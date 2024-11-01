4ec8d8e362e5af75219cf10bf536330c14617300
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-10-31 21:00:55 -0400

    5-08

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ali Elouasbi
+ * Created on: Oct 2024
+ * This program controls a wheel
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+
+// loop forever
+while (true) {
+
+// Button A functionality
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B2) 
+        basic.showIcon(IconNames.Happy)
+    }
+
+    // Button B functionality
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showIcon(IconNames.Yes)
+        robotbit.StpCarMove(10, 48)
+        basic.pause(500) 
+        robotbit.StpCarMove(-10, 48)
+        basic.showIcon(IconNames.Happy)
+    }
+}
